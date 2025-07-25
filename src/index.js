001e40ce5bfbc6496dfd48b57c17bbbb681b1f31
Dan Tisdale <dant.grnt@gmail.com>
2025-07-24 17:05:08 -0700

    reset the index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-# Create src/index.js
-echo 'import React from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
@@ -13,21 +12,4 @@ root.render(
 );
 
 serviceWorkerRegistration.register();' > src/index.js
-
-# Create src/App.js (basic version)
-echo 'import React from "react";
-import { Search, Plus } from "lucide-react";
-import "./App.css";
-
-function App() {
-  return (
-    <div className="App">
-      <h1>ข่าวดี Thai: Good News</h1>
-    </div>
-  );
-}
-
-export default App;' > src/App.js
-
-# Create empty CSS files
-touch src/index.css src/App.css
+();
---
+++ workbox-config 2.js
@@ -0,0 +1,6 @@
+module.exports = {
+  globDirectory: 'build/',
+  globPatterns: ['**/*.{js,css,html,png,svg}'],
+  swSrc: 'src/service-worker.js',
+  swDest: 'build/service-worker.js'
+};
