252a3a1f307de4f3339d9a6758e4a497d70abc8d
Dan Tisdale <dant.grnt@gmail.com>
2025-10-06 19:14:10 -0700

    Update vite.config.ts
    version issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
-// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { readFileSync } from 'node:fs';
 
-const pkg = JSON.parse(
-  readFileSync(new URL('./package.json', import.meta.url), 'utf8')
-);
+const pkg = JSON.parse(readFileSync(new URL('./package.json', import.meta.url), 'utf8'));
 
 export default defineConfig({
   plugins: [react()],
